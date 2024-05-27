dd200b9db0faada4282dad7b847feb16c4786576
15DanBerg <daniel.berg031@gmail.com>
2024-05-27 11:13:54 -0300

    chore: correct list return

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,12 @@
 from selenium.webdriver import Chrome
+from domain.book import Book
+from typing import List
 
 class Bot():
     def __init__(self, driver: Chrome):
         self.__driver = driver
     
-    def scrape(self, page_number: int = None) -> (int, List[Property]):
+    def scrape(self, page_number: int = None) -> (int, List[Book]):
         raise NotImplementedError(
             "method scrape has not implemented on %s" % (self.__class__.__name__)
         )
