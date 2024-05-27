71fb66b9a5ea5ab56a128652649671df9e97e783
15DanBerg <daniel.berg031@gmail.com>
2024-05-27 10:10:22 -0300

    chore: created domain class

---
+++ domain/book.py
@@ -0,0 +1,18 @@
+class book():
+    title = None
+    price = None
+    price_with_rate = None
+    universal_product_code = None
+    stok = None
+    reviews = None
+    
+    @classmethod
+    def from_dict(cls, args):
+        return cls(**args)
+    
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+            
+    def to_dict(self):
+        return {key: getattr(self, key) for key in self.__dict__ if not key.startswith('__')}
