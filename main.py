dc4bf04e5d042a2c8f6912f6057a991e55eaeace
15DanBerg <daniel.berg031@gmail.com>
2024-05-27 10:55:40 -0300

    chore: import bot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+from core.bot.botbook import BookBot
+
 def get_driver() -> webdriver.Chrome:
     driver = webdriver.Chrome()
     driver.set_page_load_timeout(90)
@@ -8,3 +10,4 @@ def get_driver() -> webdriver.Chrome:
 
 if __name__ == '__main__':
     driver = get_driver()
+    bot = BookBot(driver)
