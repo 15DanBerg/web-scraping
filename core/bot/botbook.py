1f70215a5c62a07fa6ee8b17292d5639800dba05
15DanBerg <daniel.berg031@gmail.com>
2024-05-27 11:13:26 -0300

    chore: method that checks if there are more pages

--- core/bot/botbook.py
+++ core/bot/botbook.py
@@ -1,16 +1,27 @@
-from selenium.webdriver import Chrome
 from core import Bot
+from domain.book import Book
 
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+from typing import List
 
 class BookBot(Bot):
     def __init__(self, driver: Chrome):
         self.url = "https://books.toscrape.com/"
         self.__driver = driver
     
-    def scrape(self, page_number: int = None) -> (int, List[Property]):
+    def scrape(self, page_number: int = None) -> (int, List[Book]):
         if self.__driver.title == "":
-            return None
+            return 
     
     def has_more_pages(self) -> bool:
-        self
-    
+        if self.__driver.title == "":
+            return True
+        
+        next_page_button_xpath = '//li[@class="next"]'
+        if self.__driver.find_element(By.XPATH, next_page_button_xpath).is_displayed():
+            return True
+
+        return False
+        
